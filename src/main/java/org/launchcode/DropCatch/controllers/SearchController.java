b8eae1827fff0d712521d75302ae22cb076c8728
Not-Ewe <80078053+Not-Ewe@users.noreply.github.com>
2021-09-28 14:26:22 -0500

    FIxed search controller further

--- src/main/java/org/launchcode/DropCatch/controllers/SearchController.java
+++ src/main/java/org/launchcode/DropCatch/controllers/SearchController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.expression.Lists;
 
+import java.util.List;
 import java.util.Locale;
 
 @Controller
@@ -29,20 +31,31 @@ public class SearchController {
 
         Iterable<PinballMachines> machines;
 
+        // Search all
         if (searchType.equals("all") || searchTerm.equals("") || searchTerm.toLowerCase(Locale.ROOT).equals("all")){
             machines = pinballMachineRepository.findAll();
             model.addAttribute("machines", machines);
             return "search/results";
         }
+        // Search by Machine Name
         if (searchType.equals("machineName")) {
             machines = pinballMachineRepository.findByMachineNameLike("%" +
                             searchTerm.toLowerCase(Locale.ROOT) + "%");
+            if (!machines.iterator().hasNext()) {
+                model.addAttribute("errors", "No matches found");
+                return "search/results";
+            }
             model.addAttribute("machines", machines);
             return "search/results";
         }
+        // Search by Machine Location
         if (searchType.equals("machineLocation")) {
             machines = pinballMachineRepository.findByMachineLocationLike("%" +
                     searchTerm.toLowerCase(Locale.ROOT) + "%");
+            if (!machines.iterator().hasNext()) {
+                model.addAttribute("errors", "No matches found");
+                return "search/results";
+            }
             model.addAttribute("machines", machines);
 
             return "search/results";
